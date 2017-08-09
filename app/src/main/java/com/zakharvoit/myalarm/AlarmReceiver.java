57e9c161a7659d3f8fdc7e56975c29f495f3dc46
za.voyt <za.voyt@region.tensor.ru>
2017-08-09 13:12:57 +0300

    Add location tracking.

--- app/src/main/java/com/zakharvoit/myalarm/AlarmReceiver.java
+++ app/src/main/java/com/zakharvoit/myalarm/AlarmReceiver.java
@@ -3,22 +3,134 @@ package com.zakharvoit.myalarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.util.Date;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class AlarmReceiver extends BroadcastReceiver {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class AlarmReceiver extends BroadcastReceiver implements LocationListener, GoogleApiClient.ConnectionCallbacks {
+    public static final long DEBOUNCE_TIMEOUT = 1000 * 60;
+
+    private GoogleApiClient mGoogleApiClient;
+    private PowerManager.WakeLock mLock;
+    private Context mContext;
+    private AtomicBoolean mInProgress;
+    private AtomicBoolean mIsConnected;
+    private long mLastReceived = 0;
+
+    public AlarmReceiver(Context context) {
+        mInProgress = new AtomicBoolean(false);
+        mIsConnected = new AtomicBoolean(false);
+        mContext = context;
+        initGoogleApi();
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (!acquireLock(context)) {
+            return;
+        }
+        long currentTime = SystemClock.elapsedRealtime();
+        if (!mIsConnected.get() || (currentTime - mLastReceived) < DEBOUNCE_TIMEOUT) {
+            releaseLock();
+            return;
+        }
+        mLastReceived = currentTime;
+        requestLocationUpdates();
+    }
+
+    private boolean acquireLock(Context context) {
+        if (!mInProgress.compareAndSet(false, true)) {
+            return false;
+        }
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        mLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "MyWakelockTag");
+        mLock.acquire();
+        return true;
+    }
+
+    void initGoogleApi() {
+        mGoogleApiClient = new GoogleApiClient.Builder(mContext)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    void requestLocationUpdates() {
+        LocationRequest locationRequest = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(5 * 1000);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(mContext, "Location callback received!", Toast.LENGTH_SHORT).show();
+        Log.d("!!!Location!!!", "" + location);
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Ringtone r = RingtoneManager.getRingtone(context, notification);
+        Ringtone r = RingtoneManager.getRingtone(mContext, notification);
         r.play();
 
-        Toast.makeText(context, "Alarm received!", Toast.LENGTH_SHORT).show();
-        Log.d("!!!ALARM!!!", " date:" + new Date());
+        Intent intent = new Intent(mContext, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP |  Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("Location", location);
+        dumpToLogFile(mContext, location);
+        mContext.startActivity(intent);
+
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        releaseLock();
+    }
+
+    private void dumpToLogFile(Context context, Location location) {
+        try {
+            String path = context.getApplicationInfo().dataDir;
+            File log = new File(path, "tracking_log.txt");
+            PrintWriter writer = new PrintWriter(new FileOutputStream(log, true));
+            writer.write(String.format("lat: %.6f lon: %.6f acc: %.1f time: %s\n",
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    location.getAccuracy(),
+                    SimpleDateFormat.getTimeInstance().format(new Date(location.getTime()))));
+            writer.close();
+        } catch (FileNotFoundException ignored) {
+        }
+    }
+
+    private void releaseLock() {
+        mLock.release();
+        mInProgress.set(false);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(mContext, "Google api connected!", Toast.LENGTH_SHORT).show();
+        mIsConnected.set(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // should not get here
     }
 }
--- app/src/main/java/com/zakharvoit/myalarm/MainActivity.java
+++ app/src/main/java/com/zakharvoit/myalarm/MainActivity.java
@@ -1,33 +1,82 @@
 package com.zakharvoit.myalarm;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    Button mStartTrackingButton;
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 111;
+    private Button mStartTrackingButton;
+    private ListView mListOfLocations;
+    private List<String> mLocations = new ArrayList<>();
+    private ArrayAdapter<String> mLocationsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkLocationAllowed();
+
         mStartTrackingButton = (Button) findViewById(R.id.start_tracking_button);
+        mListOfLocations = (ListView) findViewById(R.id.list_of_locations);
 
         mStartTrackingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Context context = getApplicationContext();
-                Toast.makeText(context, "Tracking started", Toast.LENGTH_SHORT).show();
-                IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-                filter.setPriority(1000);
-                context.registerReceiver(new AlarmReceiver(), filter);
+                requestAlarm(getApplicationContext());
             }
         });
+
+        mLocationsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mLocations);
+        mListOfLocations.setAdapter(mLocationsAdapter);
+    }
+
+    static void requestAlarm(Context context) {
+        Toast.makeText(context, "Tracking started", Toast.LENGTH_SHORT).show();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        filter.setPriority(1000);
+        context.registerReceiver(new AlarmReceiver(context), filter);
+    }
+
+    void checkLocationAllowed() {
+        // Check for permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this, // Activity
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Location location = intent.getParcelableExtra("Location");
+        mLocationsAdapter.add(
+                String.format("lat: %.6f lon: %.6f time: %s",
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        SimpleDateFormat.getTimeInstance().format(new Date(location.getTime()))));
     }
 }
